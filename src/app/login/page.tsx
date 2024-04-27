ae63daff4607f4ef4635bbe14fd2860879a1e47a
MoayadMaher <its.moayadj@gmail.com>
2024-04-27 13:37:34 +0300

    sign out button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import GoogleButton from "react-google-button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
+  const handleSignIn = () => {
+    signIn("google");
+    redirect("/");
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -11,7 +17,13 @@ export default function LoginPage() {
           It’s a good choice enjoy the website and erase my number
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <GoogleButton onClick={() => signIn("google")} />
+          <GoogleButton onClick={handleSignIn} />
+          <button
+            onClick={() => signOut()}
+            className="rounded-md bg-white px-4 py-2 text-black"
+          >
+            Back to My live
+          </button>
         </div>
       </div>
     </main>
