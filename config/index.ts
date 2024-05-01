7164630b4f34572f344b1731eb0b04889eab52fc
MoayadMaher <its.moayadj@gmail.com>
2024-05-02 00:47:18 +0300

    redirect logged in user to home

---
+++ config/index.ts
@@ -0,0 +1,1 @@
+export const NEXT_URL = process.env.NEXT_URL || "http://localhost:3000";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+import { NEXT_URL } from "../config";
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   console.log(token);
   const { pathname } = req.nextUrl;
 
+  if (token && pathname.startsWith("/login")) {
+    return NextResponse.redirect(NEXT_URL);
+  }
+
   if (!token && pathname.startsWith("/services")) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/api/auth/signin";
-    return NextResponse.redirect(url);
+    // const url = req.nextUrl.clone();
+    return NextResponse.redirect(`${NEXT_URL}/login`);
   }
 
   return NextResponse.next();
