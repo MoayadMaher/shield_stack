c50dd8123a1e5f64fccc8e9567fa3761098be638
MoayadMaher <its.moayadj@gmail.com>
2024-05-22 03:05:13 +0300

    small fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar({ session }: any) {
             ) : (
               <li>
                 <Link
-                  href="/api/auth/signin"
+                  href="/login"
                   className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
                 >
                   Login
